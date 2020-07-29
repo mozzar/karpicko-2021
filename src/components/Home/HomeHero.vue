139e559ba95056433f80a49d2d274d00e8d89323
matostr98 <matostr98@gmail.com>
2020-07-29 18:44:17 +0200

    added paralax

---
+++ src/components/Home/HomeHero.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="hero">
+    <v-parallax dark src="@/assets/karpicko_hero.jpg" height="700">
+      <v-row align="center" justify="center">
+        <v-col class="text-center" cols="12">
+<!--          <v-img src="@/assets/FUT_poznan.png" contain max-height="300"></v-img>-->
+          <h2 class="display-0 font-weight-thin mb-4">
+            Obóz integracyjno-szkoleniowy - Karpicko 2020
+          </h2>
+          <h3 class="subheading">Zapraszamy!</h3>
+        </v-col>
+      </v-row>
+    </v-parallax>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HomeHero"
+};
+</script>
+
+<style scoped></style>
