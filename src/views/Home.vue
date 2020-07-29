0f390a76e0953271ed90eab5f5907727c9fdc977
matostr98 <matostr98@gmail.com>
2020-07-29 18:48:42 +0200

    deleted default vue component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,17 @@
 <template>
   <div class="home">
     <home-hero />
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
 import HomeHero from "../components/Home/HomeHero";
 
 export default {
   name: "Home",
   components: {
-    HomeHero,
-    HelloWorld
+    HomeHero
   }
 };
 </script>
