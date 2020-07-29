67fea696a9da38b26cc41260caf18a49165f8284
matostr98 <matostr98@gmail.com>
2020-07-29 17:52:36 +0200

    repo init

---
+++ src/components/KrFooter.vue
@@ -0,0 +1,39 @@
+<template>
+  <v-footer dark padless>
+    <link
+      href="https://use.fontawesome.com/releases/v5.0.13/css/all.css"
+      rel="stylesheet"
+    />
+    <v-card flat tile class="primary white--text text-center" width="100%">
+      <v-card-text>
+        <v-btn v-for="icon in icons" :key="icon" class="mx-4 white--text" icon>
+          <v-icon size="24px">{{ icon }}</v-icon>
+        </v-btn>
+      </v-card-text>
+
+      <v-card-text class="white--text pt-6">
+        Niniejszy serwis znajduje się na serwerze Politechniki Poznańskiej. Za
+        jego treść odpowiedzialny jest Samorząd Studentów Politechniki
+        Poznańskiej. Kontakt do administratora podany jest w zakładce Kontakt.
+      </v-card-text>
+
+      <v-divider></v-divider>
+
+      <v-card-text class="white--text">
+        &copy;{{ new Date().getFullYear() }} —
+        <strong>Samorząd Studentów Politechniki Poznańskiej</strong>
+      </v-card-text>
+    </v-card>
+  </v-footer>
+</template>
+
+<script>
+export default {
+  name: "KrFooter",
+  data: () => ({
+    icons: ["fab fa-facebook", "fab fa-instagram"]
+  })
+};
+</script>
+
+<style scoped></style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,84 @@
+<template>
+  <span>
+    <v-app-bar app color="primary" dark flat>
+      <v-app-bar-nav-icon
+        class="hidden-lg-and-up"
+        @click="drawer = !drawer"
+      ></v-app-bar-nav-icon>
+      <v-spacer class=" hidden-lg-and-up"></v-spacer>
+      <div class="d-flex align-center" @click="$vuetify.goTo(main.id)">
+        <v-img
+          alt="sspp-name"
+          contain
+          min-width="10"
+          src="@/assets/SSPP_white.png"
+          width="85"
+        />
+      </div>
+      <v-spacer />
+      <div v-for="(item, index) in items" :key="index">
+        <v-btn
+          text
+          class="hidden-md-and-down"
+          elevation="0"
+          @click="$vuetify.goTo(item.id)"
+          >{{ item.title }}</v-btn
+        >
+      </div>
+    </v-app-bar>
+
+    <v-navigation-drawer
+      app
+      v-model="drawer"
+      class="primary"
+      dark
+      disable-resize-watcher
+    >
+      <v-list>
+        <v-list-item @click="$vuetify.goTo(main.id)">
+          <div class="d-flex align-center">
+            <v-img
+              alt="Fut-name"
+              contain
+              min-width="100"
+              src="@/assets/FUT_white.png"
+              width="200"
+            />
+          </div>
+        </v-list-item>
+        <v-list-item
+          v-for="item in items"
+          :key="item.title"
+          @click="$vuetify.goTo(item.id)"
+        >
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </span>
+</template>
+
+<script>
+export default {
+  name: "Navbar",
+  data() {
+    return {
+      appTitle: "Karpicko 2020",
+      drawer: false,
+      main: { title: "Karpicko 2020", id: "#hero" },
+      items: [
+        { title: "Zaproszenie", id: "#invitation" },
+        { title: "Rejestracja", id: "#registration" },
+        { title: "Harmonogram", id: "#agenda" },
+        { title: "Kontakt", id: "" },
+        { title: "Zakwaterowanie", id: "" },
+        { title: "Partnerzy", id: "" }
+      ]
+    };
+  }
+};
+</script>
+
+<style scoped></style>
